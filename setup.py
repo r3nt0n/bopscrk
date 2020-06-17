49c7e24fa55717a035a0aa2b4968776495a785c9
r3nt0n <r3nt0n@protonmail.com>
2020-06-18 00:50:11 +0200

    2.0 version: Python 3 is now supported. Fixing #4, #5 and #6 issues. Special thanks to @agoertz-fls

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Bopscrk',
+    author='r3nt0n',
+    version='2.0',
+    packages=['bopscrk',],
+    license='GNU General Public License v3.0',
+)
