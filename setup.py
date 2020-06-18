6509b484472fd83537b7dff0861630dd90b0f775
r3nt0n <r3nt0n@protonmail.com>
2020-06-18 18:46:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='Bopscrk',
+    name='bopscrk',
     author='r3nt0n',
+    author_email='bvega@protonmail.com',
+    url='https://github.com/r3nt0n/bopscrk',
     version='2.0',
-    packages=['bopscrk',],
     license='GNU General Public License v3.0',
+    packages=['bopscrk',],
+    install_requires=['requests','bs4',]
 )
