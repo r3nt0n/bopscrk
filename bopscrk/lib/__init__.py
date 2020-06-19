b20927d9709d6b0a919f9dfb92d6723acbdced4d
r3nt0n <r3nt0n@protonmail.com>
2020-06-19 14:11:32 +0200

    Fixing setup.py

--- bopscrk/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# bopscrk (https://www.github.com/R3nt0n/bopscrk)
-# R3nt0n (https://www.github.com/R3nt0n)
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# r3nt0n 25/10/2017
+# last update: 17/06/2020
+
+
+from setuptools import setup, find_packages
 
 setup(
     name='bopscrk',
@@ -7,6 +13,7 @@ setup(
     url='https://github.com/r3nt0n/bopscrk',
     version='2.0',
     license='GNU General Public License v3.0',
-    packages=['bopscrk',],
-    install_requires=['requests','beautifulsoup4',]
+    packages=['lib',],
+    scripts=['bopscrk.py'],
+    install_requires=['requests','beautifulsoup4'],
 )
