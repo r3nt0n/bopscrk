34fb3fd06f969775d8ed8ba5056b38419facec51
r3nt0n <r3nt0n@protonmail.com>
2020-11-17 12:02:40 +0100

    encoding color codes to utf8 (now color should be displayed on windows systems too)

--- modules/color.py
+++ modules/color.py
@@ -5,19 +5,19 @@
 from random import choice
 
 class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ORANGE = '\033[33m'
-    GREY = '\033[90m'
+    PURPLE = u'\033[95m'
+    CYAN = u'\033[96m'
+    DARKCYAN = u'\033[36m'
+    BLUE = u'\033[94m'
+    GREEN = u'\033[92m'
+    YELLOW = u'\033[93m'
+    RED = u'\033[91m'
+    BOLD = u'\033[1m'
+    UNDERLINE = u'\033[4m'
+    ORANGE = u'\033[33m'
+    GREY = u'\033[90m'
     #ORANGEBG = '\033[48;2;255;165;0m'
-    END = '\033[0m'
+    END = u'\033[0m'
 
     RAND_KEY_COLOR = [PURPLE, CYAN, DARKCYAN, YELLOW, ORANGE]
     KEY_HIGHL = choice(RAND_KEY_COLOR)
