715cb41d87d0b7582fb36d59c9c2a6b4c46030bd
r3nt0n <r3nt0n@protonmail.com>
2022-07-27 14:14:23 +0200

    - Fixing setup.py to include Pypi project description

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@
 
 from setuptools import setup, find_packages
 
+# Read project description
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
 setup(
     name='bopscrk',
     author='r3nt0n',
     author_email='r3nt0n@protonmail.com',
     url='https://github.com/r3nt0n/bopscrk',
-    version='2.4.1',
+    version='2.4.2',
     license='GNU General Public License v3.0',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     #packages=['modules',],
     packages=find_packages(),
     scripts=['bopscrk.py'],
