adb20506ab7bda7d53512e25439fc2873bd5368a
r3nt0n <25709767+r3nt0n@users.noreply.github.com>
2024-04-06 10:45:10 +0200

    Updating submodule

--- build_package.sh
+++ build_package.sh
@@ -1,1 +1,6 @@
+# Update submodule (https://stackoverflow.com/a/1032653)
+#git submodule update --init --recursive  # only the first time
+git submodule update --recursive --remote
+
+# Build package
 python3 setup.py -v sdist
